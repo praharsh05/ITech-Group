6f9f4197dedfb8c893e64c9c71fd552be2217acb
praharsh05 <praharsh.dubey05@gmail.com>
2023-03-03 01:40:18 +0000

    #7 url path for login, logout and dashboard

--- simplify_project/simplify_main_app/urls.py
+++ simplify_project/simplify_main_app/urls.py
@@ -4,5 +4,8 @@ from simplify_main_app import views
 app_name='simplify_main_app'
 urlpatterns= [
     path('', views.index, name='index'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('login/', views.user_login, name='login'),
+    path('dashboard/',views.dashboard, name='dashboard'),
+    path('logout/', views.user_logout, name='logout'),
 ]
