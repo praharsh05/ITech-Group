30b25b9189f7e87c78896363ef402a9f004b5ee2
praharsh05 <praharsh.dubey05@gmail.com>
2023-03-03 01:39:30 +0000

    #7 login form.py

--- simplify_project/simplify_main_app/forms.py
+++ simplify_project/simplify_main_app/forms.py
@@ -2,15 +2,21 @@ from django import forms
 from django.contrib.auth.models import User
 from simplify_main_app.models import UserProfile
 
+
+#creating a user form to get details
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)#using the widget to hide password on input
+
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('username', 'email', 'password','first_name', 'last_name', )
 
+#creating another form to get the type
 class UserProfileForm(forms.ModelForm):
 
     class Meta:
         model = UserProfile
-        fields = ('firstName', 'lastName', 'type',)
+        fields = ('type',)
