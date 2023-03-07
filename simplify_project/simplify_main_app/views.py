14c98cd317bd54d0daf2e173ea47233d39a80ea1
praharsh05 <praharsh.dubey05@gmail.com>
2023-03-07 00:12:46 +0000

    #13 changes in the view for new model

--- simplify_project/simplify_main_app/views.py
+++ simplify_project/simplify_main_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from simplify_main_app.forms import UserForm, UserProfileForm
+from simplify_main_app.forms import UserForm
 from django.contrib.auth import authenticate, login,logout
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
@@ -19,25 +19,18 @@ def register(request):
     registered=False
     if request.method == 'POST':
         user_form = UserForm(request.POST)
-        profile_form = UserProfileForm(request.POST)
 
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
+        if user_form.is_valid():
+            user = user_form.save(False)
             user.set_password(user.password)
             user.save()
-
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
             registered=True
         else:
-            print(user_form.errors, profile_form.errors)
+            print(user_form.errors)
     else:
         user_form = UserForm()
-        profile_form = UserProfileForm()
 
     return render(request, 'simplify_main_app/register.html',context={ 'user_form':user_form,
-                                                                      'profile_form':profile_form,
                                                                       'registered':registered})
 
 #Login view
