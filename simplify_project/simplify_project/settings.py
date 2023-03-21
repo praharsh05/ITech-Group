6742ad3fedd9869c7736445ffd1c141345ef05d2
praharsh05 <praharsh.dubey05@gmail.com>
2023-03-21 18:42:57 +0000

    allowed hosts changes

--- simplify_project/simplify_project/settings.py
+++ simplify_project/simplify_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['praharsh05.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
